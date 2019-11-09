3c434bd4318587c9a2106ad3e192a61394d6437c
tannerdriggers <tannerdriggers@ksu.edu>
2019-11-09 12:03:50 -0600

    Authentication Fortune Telling initialization

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/authentication';
+import { withRouter } from 'react-router-dom';
+
+class Navbar extends Component {
+
+    onLogout(e) {
+        e.preventDefault();
+        this.props.logoutUser(this.props.history);
+    }
+
+    constructor(props) {
+        super(props);
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true,
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
+        const { isAuthenticated } = this.props.auth;
+        const collapsed = this.state.collapsed;
+        const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+        const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navber-toggler-right'
+        const authLinks = (
+            <ul className="navbar-nav ml-auto float-right">
+                
+                <a href="#" className="nav-link" onClick={this.onLogout.bind(this)}>
+                    Logout
+                </a>
+            </ul>
+        );
+        const guestLinks = (
+            <ul className="navbar-nav ml-auto float-right">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Sign Up</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Sign In</Link>
+                </li>
+            </ul>
+        );
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">Fortunes</Link>
+                <button onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`${classOne}`} id="navbarSupportedContent">
+                    {isAuthenticated ? authLinks : guestLinks}
+                </div>
+            </nav>
+        )
+    }
+}
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(withRouter(Navbar));
