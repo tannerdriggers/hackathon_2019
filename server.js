1285b3ff22ec4fe3a122119b83742931a3ee5911
tannerdriggers <tannerdriggers@ksu.edu>
2019-11-09 19:00:13 -0600

    cors

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.header("Access-Control-Allow-Origin", "*");
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.use('/api/users', users);
 
