b777e6f5491c05a738a2c75c431c1362fb310cd2
tannerdriggers <tannerdriggers@ksu.edu>
2019-11-10 01:10:19 -0600

    cors

--- server.js
+++ server.js
@@ -32,12 +32,14 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-	next();
-});
+app.options('*', cors());
+
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+// 	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+// 	next();
+// });
 
 app.use('/api/users', cors(), users);
 app.use('/api/twilio', cors(), twilio);
